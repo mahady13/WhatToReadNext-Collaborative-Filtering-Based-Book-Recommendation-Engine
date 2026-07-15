00e3b3d8e2a4a897ed83e0646f2a1bdba51ca111
mahady13 <mohiuddinmahady@gmail.com>
2026-07-15 06:41:07 +0600

    added recommend functions

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ def load_assets():
     similarity=joblib.load("similarity.pkl")
     return books_with_ratings,books,pt,similarity
 books_with_ratings,books,pt,similarity=load_assets()
+
+
+def recommend_books(book):
+    indexs = np.where(pt.index == book)[0][0]
+    similar_books = sorted(list(enumerate(similarity[indexs])), key=lambda x: x[1], reverse=True)[1:6]
+    data = []
+
+    for i in similar_books:
+        item = []
+        temp_df = books_with_ratings[books_with_ratings['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')["Book-Author"].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')["Image-URL-L"].values))
+        data.append(item)
+    return data
