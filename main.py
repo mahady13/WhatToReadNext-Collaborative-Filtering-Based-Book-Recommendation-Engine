9ebb2ecddcdd10f7a612c5370e15952a72f0e918
mahady13 <mohiuddinmahady@gmail.com>
2026-07-15 06:37:47 +0600

    loaded the pre trained models

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import pandas as pd
 import numpy as np
 import joblib
 
+@st.cache_resource
+def load_assets():
+    books_with_ratings=joblib.load("preprocessed_list")
+    books=pd.read_csv("Books.csv")
+    pt=joblib.load("pivot_table.pkl")
+    similarity=joblib.load("similarity.pkl")
+    return books_with_ratings,books,pt,similarity
+books_with_ratings,books,pt,similarity=load_assets()
